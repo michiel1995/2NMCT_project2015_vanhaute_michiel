9e9e9f7d08c820ce3725e06c3cf21f786e3b72ae
michiel1995 <michiel.van.haute@student.howest.be>
2015-03-30 19:02:45 +0200

    add google map references

---
+++ app/src/main/java/be/howest/nmct/beerprice/MainFragment.java
@@ -0,0 +1,21 @@
+package be.howest.nmct.beerprice;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Michiel on 30/03/2015.
+ */
+public class MainFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_main,container, false);
+
+        return v;
+    }
+}
